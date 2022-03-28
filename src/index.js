7575293d40a764e7ec230c7d69d5bd0b00449646
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-03-28 10:47:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const app = express();
 app.use(express.json());
 
 const todoController = require("./controller/todo.controller");
-const {registerController, loginController} = require("./controller/auth.controller");
+const {register, login} = require("./controller/auth.controller");
 
 
 app.use("/todos", todoController)
 
-app.use("/register", registerController);
+app.use("/register", register);
 
-app.use("/login", loginController);
+app.use("/login", login);
 
 module.exports = app;
