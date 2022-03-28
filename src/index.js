a075a1abaddf9699be4325b8ebf794635fecd5d3
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-03-28 10:06:18 +0530

    main

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ const app = express();
 app.use(express.json());
 
 const todoController = require("./controller/todo.controller");
-const todoidController = require("./controller/todoid.controller");
 const loginController = require("./controller/login.controller");
 const registerController = require("./controller/register.controller");
 
 
 app.use("/todos", todoController)
-
-app.use("/todos:id", todoidController)
 
 app.use("/register", registerController);
 
